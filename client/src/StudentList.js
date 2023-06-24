b97b4d1d518d902c721102b768a421c0d04cd8e1
Guru charan <premguru1045@gmail.com>
2023-06-24 15:18:58 +0530

    second commit

--- client/src/StudentList.js
+++ client/src/StudentList.js
@@ -1,97 +1,65 @@
 import React, { useState } from 'react';
 import './StudentList.css';
+import SearchComponent from './SearchComponent';
 
 function StudentList({ studentList, attendanceData, handleAttendanceChange }) {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState(studentList);
 
-  const handleSearch = (e) => {
-    setSearchTerm(e.target.value);
+  const handleSearch = (results) => {
+    setSearchResults(results);
   };
-
-  const filteredStudentList = studentList.filter((student) =>
-    student.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
 
   return (
     <div className="StudentList">
-      <div className="search-bar">
-        <input
-          type="text"
-          placeholder="Search by name"
-          value={searchTerm}
-          onChange={handleSearch}
-        />
-      </div>
+      <SearchComponent
+        data={studentList}
+        searchKey="name"
+        setSearchResults={handleSearch}
+      />
+
       <table>
         <thead>
           <tr>
-            <th style={{textAlign:"center"}}>Name</th>
-            <th style={{textAlign:"center"}}>Roll Number</th>
-            <th style={{textAlign:"center"}}>Attendance</th>
+            <th style={{ textAlign: 'center' }}>Name</th>
+            <th style={{ textAlign: 'center' }}>Roll Number</th>
+            <th style={{ textAlign: 'center' }}>Attendance</th>
           </tr>
         </thead>
         <tbody>
-          {searchTerm === ''
-            ? studentList.map((student) => (
-                <tr key={student._id}>
-                  <td>{student.name}</td>
-                  <td>{student.rollNumber}</td>
-                  <td>
-                    <div className="attendance-container">
-                      <label>
-                        <input
-                          type="radio"
-                          name={`attendance-${student._id}`}
-                          value="present"
-                          checked={attendanceData[student._id] === 'present'}
-                          onChange={() => handleAttendanceChange(student._id, 'present')}
-                        />
-                        Present
-                      </label>
-                      <label>
-                        <input
-                          type="radio"
-                          name={`attendance-${student._id}`}
-                          value="absent"
-                          checked={attendanceData[student._id] === 'absent'}
-                          onChange={() => handleAttendanceChange(student._id, 'absent')}
-                        />
-                        Absent
-                      </label>
-                    </div>
-                  </td>
-                </tr>
-              ))
-            : filteredStudentList.map((student) => (
-                <tr key={student._id}>
-                  <td>{student.name}</td>
-                  <td>{student.rollNumber}</td>
-                  <td>
-                    <div className="attendance-container">
-                      <label>
-                        <input
-                          type="radio"
-                          name={`attendance-${student._id}`}
-                          value="present"
-                          checked={attendanceData[student._id] === 'present'}
-                          onChange={() => handleAttendanceChange(student._id, 'present')}
-                        />
-                        Present
-                      </label>
-                      <label>
-                        <input
-                          type="radio"
-                          name={`attendance-${student._id}`}
-                          value="absent"
-                          checked={attendanceData[student._id] === 'absent'}
-                          onChange={() => handleAttendanceChange(student._id, 'absent')}
-                        />
-                        Absent
-                      </label>
-                    </div>
-                  </td>
-                </tr>
-              ))}
+          {searchResults.map((student) => (
+            <tr key={student._id}>
+              <td>{student.name}</td>
+              <td>{student.rollNumber}</td>
+              <td>
+                <div className="attendance-container">
+                  <label>
+                    <input
+                      type="radio"
+                      name={`attendance-${student._id}`}
+                      value="present"
+                      checked={attendanceData[student._id] === 'present'}
+                      onChange={() =>
+                        handleAttendanceChange(student._id, 'present')
+                      }
+                    />
+                    Present
+                  </label>
+                  <label>
+                    <input
+                      type="radio"
+                      name={`attendance-${student._id}`}
+                      value="absent"
+                      checked={attendanceData[student._id] === 'absent'}
+                      onChange={() =>
+                        handleAttendanceChange(student._id, 'absent')
+                      }
+                    />
+                    Absent
+                  </label>
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
